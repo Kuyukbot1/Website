eb831d3b06162c2eab3a7c55c07eef5165ded823
Kuyukbot1 <un144165@gmail.com>
2024-08-26 15:22:37 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 // Cek jika ada data users di localStorage, jika tidak ada, inisialisasi dengan user default
 if (!localStorage.getItem('users')) {
     localStorage.setItem('users', JSON.stringify({
-        : { password: "", expires: null },   // Admin tanpa tanggal kadaluarsa
+        ad: { password: "ad", expires: null },   // Admin tanpa tanggal kadaluarsa
         anonymous: { password: "anonymous", expires: null }  // User default tanpa tanggal kadaluarsa
     }));
 }
